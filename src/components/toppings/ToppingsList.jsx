4fb42262e14a7d99d23bac1eacacf0ce4be047ca
Katherine Tam <katherinemtam@gmail.com>
2021-08-06 16:12:12 -0700

    ToppingList component

---
+++ src/components/toppings/ToppingsList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useToppings } from '../state/topping';
+import { Link } from 'react-router-dom';
+import Topping from './Toppings';
+
+const ToppingsList = () => {
+  const toppings = useToppings();
+  console.log(toppings);
+
+  const toppingElements = toppings.map(topping => (
+    <li key={topping.id}>
+      <Link to={`/${topping.id}`}>
+        <Topping {...topping}/>
+      </Link>
+    </li>
+  ));
+  return (
+    <>
+      <h1>Toppings</h1>
+      <ul>{toppingElements}</ul>
+    </>
+  );
+};
+
+export default ToppingsList;
