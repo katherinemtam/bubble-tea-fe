3a9844ecf4b84749799f822653f480e995a8122e
Katherine Tam <katherinemtam@gmail.com>
2021-08-07 17:00:07 -0700

    fetchTopping function

--- src/components/services/toppingsApi.js
+++ src/components/services/toppingsApi.js
@@ -1,6 +1,11 @@
 export const fetchToppings = async () => {
   const res = await fetch('https://bubble-tea-be.herokuapp.com/api/v1/toppings');
   const json = res.json();
-  console.log('fetching', json);
+  return json;
+};
+
+export const fetchTopping = async (id) => {
+  const res = await fetch(`https://bubble-tea-be.herokuapp.com/api/v1/toppings/${id}`);
+  const json = res.json();
   return json;
 };
