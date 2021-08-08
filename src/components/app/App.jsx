86ce8e9f53dbbad7ce127791880f0eddbfbe04c9
Katherine Tam <katherinemtam@gmail.com>
2021-08-07 22:18:32 -0700

    add form route

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ToppingDetails from '../details/ToppingDetails';
+import AddTopping from '../forms/AddToppings';
 import ToppingsList from '../toppings/ToppingsList';
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <>
       <Switch>
         <Route exact path="/" component={ToppingsList}/>
+        <Route exact path="/add" component={AddTopping}/>
         <Route exact path="/topping/:id" component={ToppingDetails} />
       </Switch>
     </>
