b3a5769796dc264dc53cd172b01a5882e88e2953
Katherine Tam <katherinemtam@gmail.com>
2021-08-08 21:34:51 -0700

    add edit page route

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ToppingDetails from '../details/ToppingDetails';
 import AddTopping from '../forms/AddToppings';
+import EditToppings from '../forms/EditToppings';
 import ToppingsList from '../toppings/ToppingsList';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={ToppingsList}/>
         <Route exact path="/toppings/add" component={AddTopping}/>
+        <Route exact path="/toppings/:id/edit" component={EditToppings} />
         <Route exact path="/toppings/:id" component={ToppingDetails} />
       </Switch>
     </>
