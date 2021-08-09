2f3ae6de2ca7087a3b2e70251f9a0cbef8280431
Katherine Tam <katherinemtam@gmail.com>
2021-08-08 23:12:56 -0700

    adjust routes, add header and home

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,13 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 import ToppingDetails from '../details/ToppingDetails';
 import AddTopping from '../forms/AddToppings';
 import EditToppings from '../forms/EditToppings';
+import Home from '../home/Home';
 import ToppingsList from '../toppings/ToppingsList';
+import Header from './Header';
 
 export default function App() {
   return (
     <>
+      <Header/>
       <Switch>
-        <Route exact path="/" component={ToppingsList}/>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/toppings" component={ToppingsList}/>
         <Route exact path="/toppings/add" component={AddTopping}/>
         <Route exact path="/toppings/:id/edit" component={EditToppings} />
         <Route exact path="/toppings/:id" component={ToppingDetails} />
